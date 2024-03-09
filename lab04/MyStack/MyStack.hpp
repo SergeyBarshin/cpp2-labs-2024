b458eaa3530f55382e829a98a256b723f28c0471
SergeyBarshin <sergey.barshin@yandex.ru>
2024-03-10 00:19:41 +0300

    lab04 code

--- lab04/MyStack/MyStack.hpp
+++ lab04/MyStack/MyStack.hpp
@@ -1,66 +1,82 @@
 // Файл MyStack.h
 // Шаблонный класс MyStack на основе односвязного списка.
-#ifndef MyStack_h           // защита от повторной компиляции
-#define MyStack_h           // модуль подключен
+#ifndef MyStack_h // защита от повторной компиляции
+#define MyStack_h // модуль подключен
 #include <iostream>
+
 // Шаблонный класс ListNode (узел односвязного списка)
-template<class INF, class FRIEND>
-class ListNode{            // узел списка
+template <class INF, class FRIEND>
+class ListNode
+{ // узел списка
 private:
-    INF d;                  // информационная часть узла
-    ListNode *next;        // указатель на следующий узел списка
-    ListNode(void) { next = nullptr; } //конструктор
-   //ListNode(INF n) { d = n; } 
+    INF d;                             // информационная часть узла
+    ListNode *next;                    // указатель на следующий узел списка
+    ListNode(void) { next = nullptr; } // конструктор
+    ~ListNode(void) { delete next; }
+    // ListNode(INF n) { d = n; }
     friend FRIEND;
 };
 
 // Шаблонный класс MyStack на основе односвязного списка.
-template<class INF>
-class MyStack {
-    typedef class ListNode<INF, MyStack<INF> > Node;
+template <class INF>
+class MyStack
+{
+    typedef class ListNode<INF, MyStack<INF>> Node;
     Node *top;
-public:
-    MyStack(void) {  // конструктор
-        top = nullptr;
-    }      
 
-    ~MyStack(void) {  // освободить динамическую память
-        while(!empty()){
+public:
+    MyStack(void)
+    { // конструктор
+        top = nullptr;
+    }
+
+    ~MyStack(void)
+    { // освободить динамическую память
+        while (!empty())
+        {
             pop();
         }
-    }    
+    }
 
-    bool empty(void) {  // стек пустой?
+    bool empty(void)
+    { // стек пустой?
         return top == nullptr;
-    }      
-    
-    void push(INF n) { // добавить узел в вершину стека
+    }
+
+    void push(INF n)
+    { // добавить узел в вершину стека
         Node *temp = new Node;
         temp->d = n;
         temp->next = top;
         top = temp;
-       // top = top->next;
+        // top = top->next;
     }
 
-    void pop(void) { // удалить узел из вершины стека
-        if (empty()) {
+    void pop(void)
+    { // удалить узел из вершины стека
+        if (empty())
+        {
             std::cout << "Стек пуст, невозможно получить значение!" << std::endl;
             return;
-        } else {
-            Node* temp;
+        }
+        else
+        {
+            Node *temp;
             temp = top;
             top = top->next;
             delete temp;
         }
-    }   
-         
-    INF top_inf(void) { // считать информацию из вершины стека
-        if (empty()) {
+    }
+
+    INF top_inf(void)
+    { // считать информацию из вершины стека
+        if (empty())
+        {
             std::cout << "Стек пуст, невозможно получить значение!" << std::endl;
             return -1;
-        } 
+        }
         return top->d;
-    }      
+    }
 };
 
 #endif
--- lab04/main.cpp
+++ lab04/main.cpp
@@ -1,21 +1,58 @@
 #include "MyStack/MyStack.hpp"
 
+template <class DATA>
+void Multipliers(int n, MyStack<DATA> &stack)
+{
+    while (n % 2 == 0)
+    {
+        stack.push(2);
+        n = n / 2;
+    }
 
-int main(){
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            stack.push(i);
+            n = n / i;
+        }
+    }
+
+    if (n > 2)
+        stack.push(n);
+}
+
+int main()
+{
+    int n = 3960;
     MyStack<int> stack;
+    MyStack<int> stackReverse; // стек в который мы перезапишем значения
 
-    //std::cout << stack.empty() << '\n';
-    stack.push(1);
-    std::cout << stack.top_inf() << '\n';
-    stack.push(2);
-    stack.push(3);
-    std::cout << stack.top_inf() << '\n';
-    stack.pop();
-    std::cout << stack.top_inf() << '\n';
-    stack.pop();
-    stack.pop();
-    std::cout << stack.empty() << '\n';
+    Multipliers<int>(n, stack);
+    std::cout << n << "=";
+    while (!stack.empty())
+    {
+        std::cout << stack.top_inf();
+        stackReverse.push(stack.top_inf());
+        stack.pop();
 
+        if (!stack.empty())
+            std::cout << " * ";
+    }
+    std::cout << std::endl;
+
+    std::cout << n << "=";
+    while (!stackReverse.empty())
+    {
+        std::cout << stackReverse.top_inf();
+        stackReverse.pop();
+        if (!stackReverse.empty())
+            std::cout << " * ";
+    }
+    std::cout << std::endl;
+
+    // std::cout << stack.top_inf() << '\n';
+    //  std::cout << stack.empty() << '\n';
 
     return 0;
 }
